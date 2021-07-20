a26b8e0e1369f61da9c0463519e9516d320d5491
shazz <github@metaverse.fr>
2021-07-20 16:39:45 -0400

    Update run_image.sh

--- run_image.sh
+++ run_image.sh
@@ -3,3 +3,7 @@ mkdir shared
 sudo docker rm -f ipfs
 sudo docker build -t ipfs:rpi .
 sudo docker run --rm -d --name ipfs -v ${PWD}/shared:/shared -v ${PWD}/repo:/data/ipfs -p 18080:8080 -p 14001:4001 -p 15001:5001 ipfs:rpi
+
+# add aliases in bash
+echo "alias ipfs='sudo docker exec -it ipfs'" >> ~/.bashrc
+source ~/.bashrc
