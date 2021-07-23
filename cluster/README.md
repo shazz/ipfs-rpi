cc1ff9c03d12ab28dc6088eb5e08ca87d005fb72
Shazz <shazz.ng@gmail.com>
2021-07-23 21:15:06 +0000

    partially fixed proxy settings

---
+++ cluster/README.md
@@ -0,0 +1,7 @@
+## Start the cluster
+
+./run_cluster.sh
+
+## Notes
+
+after first boot, patch repo/cluster/service.json and modify the CLUSTER_IPFSPROXY_NODEMULTIADDRESS by the multiaddress of the local node)
--- cluster/run_cluster.sh
+++ cluster/run_cluster.sh
@@ -2,10 +2,16 @@ echo "create dirs"
 mkdir -p repo/cluster
 mkdir -p repo/ipfs
 
+echo "set secret and keep a copy"
+export CLUSTER_SECRET=$(od -vN 32 -An -tx1 /dev/urandom | tr -d ' \n')
+echo $CLUSTER_SECRET
+echo $CLUSTER_SECRET > repo/cluster/cluster.key
+
 echo "start docker compose"
-sudo docker-compose up -d
+sudo -E docker-compose up -d
 
 echo "add alias in bashrc"
 echo "alias ipfs='sudo docker exec -it ipfs ipfs'" >> ~/.bashrc
+echo "alias ipfs-cluster-ctl='sudo docker exec -it cluster ipfs-cluster-ctl'" >> ~/.bashrc
 source ~/.bashrc
 
